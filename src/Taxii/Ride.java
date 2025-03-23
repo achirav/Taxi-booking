7356617a193a8e33301c458b7bae44879283e506
achirav <achirav22@gmail.com>
2025-03-24 00:27:43 +0530

    Take drop and pickup location

--- src/Taxii/Ride.java
+++ src/Taxii/Ride.java
@@ -1,4 +1,22 @@
 package Taxii;
 
+import java.util.Scanner;
+
 public class Ride {
+    String pickup,destination;
+
+    Scanner dest=new Scanner(System.in);
+
+    public void pickup_meth(){
+        System.out.print("Set your pickup location: "); //take details of pickup loaction
+        pickup=dest.next();
+    }
+
+    public void drop_meth(){
+        System.out.print("Set your drop location: "); //take details of drop loacation
+        destination=dest.next();
+    }
+
+
+
 }
