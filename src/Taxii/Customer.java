cabd67c8236c4479eb6fef4945462ec8d2b0d67b
achirav <achirav22@gmail.com>
2025-03-23 15:53:54 +0530

    Customer details

--- src/Taxii/Customer.java
+++ src/Taxii/Customer.java
@@ -1,4 +1,27 @@
 package Taxii;
 
+import java.util.Scanner;
+
 public class Customer {
+    String Cname,Cage;
+
+    Scanner input=new Scanner(System.in);
+
+    public String Cdetails(){
+        System.out.print("Enter your name: ");
+        Cname=input.next();
+
+        System.out.print("Enter your age: ");
+        Cage=input.next();
+
+        return Cname;
+    }
+
+    public void display(String Cname){
+
+        System.out.println("***********************");
+        System.out.println("Welcome to Taxi service");
+        System.out.println("***********************");
+        System.out.println("\nWelcome "+Cname);
+    }
 }
