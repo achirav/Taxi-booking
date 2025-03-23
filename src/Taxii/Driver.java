da34eb2c8cdbcc508f8340994fc9e0d75d9c05c3
achirav <achirav22@gmail.com>
2025-03-24 01:33:42 +0530

    Auto select available driver

--- src/Taxii/Driver.java
+++ src/Taxii/Driver.java
@@ -1,4 +1,20 @@
 package Taxii;
 
+import java.util.Random;
+
 public class Driver {
+    int name;
+
+    String [] riders={"Sanath","Santha","lakmal","Sarath"};
+
+    Random rand=new Random();
+
+    public String driver(){
+
+        name=rand.nextInt(riders.length);
+
+        System.out.println("Your driver is "+riders[name]);
+
+        return riders[name];
+    }
 }
