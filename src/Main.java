2ef380790b620d79d33283cad7c723a508190184
achirav <achirav22@gmail.com>
2025-03-24 01:38:15 +0530

    Updated and passed details from driver class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
 import Taxii.Customer;
+import Taxii.Driver;
+import Taxii.Ride;
+import Taxii.Vehicle;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Customer customerDetail=new Customer();
+        Scanner inpt = new Scanner(System.in);
+        Customer customerDetail = new Customer();
+        Ride ride = new Ride();
+        Driver driv = new Driver();
 
         System.out.println("***********************");
         System.out.println("Welcome to Taxi service");
-        System.out.println("***********************");
+        System.out.println("***********************\n");
 
-        String disp=customerDetail.Cdetails();
+        String disp = customerDetail.Cdetails();
         customerDetail.display(disp);
+
+        System.out.print("Do you need a ride? ");
+        String answr = inpt.next();
+
+
+        if (answr.equalsIgnoreCase("y")) {
+            ride.pickup_meth();
+            ride.drop_meth();
+        } else {
+            System.exit(0);
+        }
+
+        String out = driv.driver();
     }
 }
