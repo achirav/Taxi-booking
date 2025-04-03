674f3198b4d50603a386733725820a6ff4fa9b74
achirav <achirav22@gmail.com>
2025-04-04 02:07:05 +0530

    Fixed some errors

--- src/Taxii/Customer.java
+++ src/Taxii/Customer.java
@@ -5,51 +5,50 @@ import java.util.Scanner;
 
 public class Customer {
     String Cname;
-    int age,x,otp;
+    int age, x, otp;
     long phone;
 
-    Scanner input=new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
-    public String Cdetails(){
+    public String Cdetails() {
         System.out.print("Enter your name: ");
-        Cname=input.next();
+        Cname = input.next();
 
         System.out.print("Enter your phone number: ");
-        phone= input.nextLong();
+        phone = input.nextLong();
 
         System.out.print("Enter your age: ");
-        age=input.nextInt();
+        age = input.nextInt();
 
         return Cname;
     }
 
-    public void display(String Cname){
+    public void display(String Cname) {
 
-        if (age>18){
-            System.out.println("Eligible for app");
-            System.out.println("\nWelcome "+Cname);
+        if (age > 18) {
+            System.out.println("\nLoading app......");
+            System.out.println("***********************");
+            System.out.println("\nWelcome " + Cname + " for our Taxi Service!\n");
+            System.out.println("***********************");
 
-            Random random=new Random();
-            x= random.nextInt(9999);
-            System.out.print("your OTP is : "+x);
+            Random random = new Random();
+            x = random.nextInt(9999);
+            System.out.print("your OTP is : " + x);
 
-            while (true){
+            while (true) {
                 System.out.print("\nEnter your OTP to access: ");
-                otp= input.nextInt();
+                otp = input.nextInt();
 
-                if (x==otp){
-                    System.out.println("You have access!!");
+                if (x == otp) {
+                    System.out.println("You have access!!\n");
                     break;
-                }else {
-                    System.out.println("Wrong OTP, Enter again: ");
-                    otp= input.nextInt();
-                    continue;
+                } else {
+                    System.out.println("Wrong OTP, Enter again! ");
                 }
             }
 
         } else {
             System.out.println("Access denied ");
         }
-
     }
 }
