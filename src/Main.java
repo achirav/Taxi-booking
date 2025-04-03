2226aadc1efaa6642d04313b930e26627d1d4a71
achirav <achirav22@gmail.com>
2025-04-04 02:05:16 +0530

    Display vehicle type

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int user_pin;
+        String vehciletype = "";
+
+
         Scanner inpt = new Scanner(System.in);
+
         Customer customerDetail = new Customer();
         Ride ride = new Ride();
         Driver driv = new Driver();
+
 
         System.out.println("***********************");
         System.out.println("Welcome to Taxi service");
@@ -25,12 +31,34 @@ public class Main {
 
 
         if (answr.equalsIgnoreCase("y")) {
+            System.out.print("Choose vehicle for ride: ");
+            vehciletype=inpt.next();
             ride.pickup_meth();
             ride.drop_meth();
         } else {
             System.exit(0);
         }
 
-        String out = driv.driver();
+        driv.driver();
+        System.out.println("You chose : "+vehciletype);
+
+        int driver_pin= driv.pin_detail();
+        System.out.println("\n***********************");
+        System.out.println("Driver's POV");
+        System.out.println("***********************");
+        System.out.print("\nEnter driver's pin: ");
+        user_pin=inpt.nextInt();
+        System.out.println("\n***********************");
+
+        while (true){
+            if (user_pin == driver_pin) {
+                System.out.print("Journey Begin!! Enjoy your ride!!");
+                break;
+            } else {
+                System.out.print("Wrong Input!! Enter again: ");
+                user_pin=inpt.nextInt();
+                continue;
+            }
+        }
     }
 }
